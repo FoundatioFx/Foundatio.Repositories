e0495efab725f1ad8adb5f1472dfdae851075a7d
Blake Niemyjski <bniemyjski@gmail.com>
2016-02-09 15:22:50 -0600

    Added support for nested facets

--- src/Core/Queries/FacetOptions.cs
+++ src/Core/Queries/FacetOptions.cs
@@ -1,9 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Foundatio.Repositories.Queries {
+    internal class FacetToken {
+        public string String { get; set; }
+        public string Nested { get; set; }
+
+        public static List<FacetToken> Tokenize(string input) {
+            var tokens = new List<FacetToken>();
+
+            if (input.Trim().Length == 0)
+                return tokens;
+
+            var nestedLevel = 0;
+            var token = new StringBuilder();
+            var nestedToken = new StringBuilder();
+            foreach (var c in input.ToCharArray()) {
+                switch (c) {
+                    case '(':
+                        if (nestedLevel > 0)
+                            nestedToken.Append(c);
+
+                        nestedLevel += 1;
+                        break;
+
+                    case ')':
+                        nestedLevel -= 1;
+
+                        if (nestedLevel > 0)
+                            nestedToken.Append(c);
+                        break;
+
+                    case ',':
+                        if (nestedLevel > 0) {
+                            nestedToken.Append(c);
+                        } else {
+                            tokens.Add(new FacetToken {
+                                String = token.ToString().Trim(),
+                                Nested = nestedToken.ToString(),
+                            });
+                            token.Clear();
+                            nestedToken.Clear();
+                        }
+
+                        break;
+
+                    default:
+                        if (nestedLevel > 0) {
+                            nestedToken.Append(c);
+                        } else {
+                            token.Append(c);
+                        }
+                        break;
+                }
+            }
+
+            tokens.Add(new FacetToken {
+                String = token.ToString().Trim(),
+                Nested = nestedToken.ToString(),
+            });
+
+            return tokens;
+        }
+    }
+
     public class FacetOptions {
-        public static FacetOptions Empty = new FacetOptions();
+        public static readonly FacetOptions Empty = new FacetOptions();
 
         public FacetOptions() {
             Fields = new List<FacetField>();
@@ -16,11 +79,12 @@ namespace Foundatio.Repositories.Queries {
                 return FacetOptions.Empty;
 
             var facetOptions = new FacetOptions();
-            var fields = facets.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var field in fields) {
-                string name = field;
+            var parsedFields = FacetToken.Tokenize(facets);
+
+            foreach (var field in parsedFields) {
+                string name = field.String;
                 int size = 25;
-                var parts = field.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                var parts = field.String.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
                 if (parts.Length == 2) {
                     name = parts[0];
                     int partSize;
@@ -28,13 +92,17 @@ namespace Foundatio.Repositories.Queries {
                         size = partSize;
                 }
 
-                facetOptions.Fields.Add(new FacetField { Field = name, Size = size });
+                facetOptions.Fields.Add(new FacetField {
+                    Field = name,
+                    Size = size,
+                    Nested = field.Nested.Length == 0 ? null : FacetOptions.Parse(field.Nested),
+                });
             }
 
             return facetOptions;
         }
 
-        static public implicit operator FacetOptions(string value) {
+        public static implicit operator FacetOptions(string value) {
             return Parse(value);
         }
     }
@@ -42,5 +110,7 @@ namespace Foundatio.Repositories.Queries {
     public class FacetField {
         public string Field { get; set; }
         public int? Size { get; set; }
+
+        public FacetOptions Nested { get; set; }
     }
 }
