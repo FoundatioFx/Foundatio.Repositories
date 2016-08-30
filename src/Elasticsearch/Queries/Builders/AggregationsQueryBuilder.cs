150080f172687060c1c1b72a2f95ddd03fa463d4
StephenCleary <scleary@learningmachine.com>
2016-08-30 12:52:19 -0400

    Attempted fix for nested aggregation fields.

--- src/Elasticsearch/Queries/Builders/AggregationsQueryBuilder.cs
+++ src/Elasticsearch/Queries/Builders/AggregationsQueryBuilder.cs
@@ -17,18 +17,37 @@ namespace Foundatio.Repositories.Elasticsearch.Queries.Builders {
                 return;
 
             var opt = ctx.GetOptionsAs<IElasticQueryOptions>();
-            if (opt?.AllowedAggregationFields?.Length > 0 && !aggregationQuery.AggregationFields.All(f => opt.AllowedAggregationFields.Contains(f.Field)))
-                throw new InvalidOperationException("All facet fields must be allowed.");
+            if (opt?.AllowedAggregationFields?.Length > 0 && !FlattenedFields(aggregationQuery.AggregationFields).All(f => opt.AllowedAggregationFields.Contains(f.Field)))
+                throw new InvalidOperationException("All aggregation fields must be allowed.");
 
-            ctx.Search.Aggregations(agg => GetAggregationDescriptor<T>(aggregationQuery));
+            ctx.Search.Aggregations(agg => GetAggregationDescriptor<T>(aggregationQuery.AggregationFields));
         }
 
-        private AggregationDescriptor<T> GetAggregationDescriptor<T>(IAggregationQuery query) where T : class {
+        private AggregationDescriptor<T> GetAggregationDescriptor<T>(IEnumerable<AggregationField> fields) where T : class {
             var descriptor = new AggregationDescriptor<T>();
-            foreach (var t in query.AggregationFields)
-                descriptor = descriptor.Terms(t.Field, s => s.Field(t.Field).Size(t.Size ?? 100));
+            foreach (var t in fields)
+                descriptor = descriptor.Terms(t.Field, s =>
+                {
+                    s = s.Field(t.Field).Size(t.Size ?? 100);
+                    if (t.Nested?.Fields.Count > 0)
+                        s = s.Aggregations(a => GetAggregationDescriptor<T>(t.Nested.Fields));
+                    return s;
+                });
 
             return descriptor;
+        }
+
+        private static IEnumerable<AggregationField> FlattenedFields(IEnumerable<AggregationField> source)
+        {
+            foreach (var field in source)
+            {
+                yield return field;
+                if (field.Nested != null)
+                {
+                    foreach (var nested in FlattenedFields(field.Nested.Fields))
+                        yield return nested;
+                }
+            }
         }
     }
 
