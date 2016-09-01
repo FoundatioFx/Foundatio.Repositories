10fcdfbe825179f66478bf74b5222878fc2b14c0
Blake Niemyjski <bniemyjski@gmail.com>
2016-09-01 13:11:50 -0500

    Fixed a bug where identity query could throw a null reference exception

--- src/Elasticsearch/Queries/Builders/IdentityQueryBuilder.cs
+++ src/Elasticsearch/Queries/Builders/IdentityQueryBuilder.cs
@@ -6,6 +6,8 @@ namespace Foundatio.Repositories.Elasticsearch.Queries.Builders {
     public class IdentityQueryBuilder : IElasticQueryBuilder {
         public void Build<T>(QueryBuilderContext<T> ctx) where T : class, new() {
             var identityQuery = ctx.GetSourceAs<IIdentityQuery>();
+            if (identityQuery == null)
+                return; 
 
             if (identityQuery.Ids != null && identityQuery.Ids.Count > 0)
                 ctx.Filter &= new IdsFilter { Values = identityQuery.Ids };
