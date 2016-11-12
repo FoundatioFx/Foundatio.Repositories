89d30b0efac60b25ba519f875ba1150b32dd7d4b
Blake Niemyjski <bniemyjski@gmail.com>
2016-11-11 18:10:32 -0600

    Added the ability to get aliases from mappings

---
+++ src/Foundatio.Repositories.Elasticsearch/Configuration/AliasMappingsVisitor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundatio.Parsers.LuceneQueries.Visitors;
+using Foundatio.Repositories.Elasticsearch.Extensions;
+using Nest;
+
+namespace Foundatio.Repositories.Elasticsearch.Configuration {
+    public class AliasMappingVisitor : NoopMappingVisitor {
+        private readonly Inferrer _inferrer;
+        private readonly Stack<AliasMapValue> _stack = new Stack<AliasMapValue>();
+
+        public AliasMappingVisitor(Inferrer inferrer) {
+            _inferrer = inferrer;
+        }
+
+        public AliasMap RootAliasMap { get; } = new AliasMap();
+
+        public override void Visit(ITextProperty property) => AddAlias(property);
+
+        public override void Visit(IKeywordProperty property) => AddAlias(property);
+
+        public override void Visit(IDateProperty property) => AddAlias(property);
+
+        public override void Visit(IBooleanProperty property) => AddAlias(property);
+
+        public override void Visit(IBinaryProperty property) => AddAlias(property);
+
+        public override void Visit(IObjectProperty property) => AddAlias(property);
+
+        public override void Visit(INestedProperty property) => AddAlias(property);
+
+        public override void Visit(IIpProperty property) => AddAlias(property);
+
+        public override void Visit(IGeoPointProperty property) => AddAlias(property);
+
+        public override void Visit(IGeoShapeProperty property) => AddAlias(property);
+
+        public override void Visit(IAttachmentProperty property) => AddAlias(property);
+
+        public override void Visit(INumberProperty property) => AddAlias(property);
+
+        public override void Visit(ICompletionProperty property) => AddAlias(property);
+
+        public override void Visit(IMurmur3HashProperty property) => AddAlias(property);
+
+        public override void Visit(ITokenCountProperty property) => AddAlias(property);
+
+        private void AddAlias(IProperty property) {
+            while (Depth > _stack.Count)
+                _stack.Pop();
+
+            string name = _inferrer.PropertyName(property.Name);
+            var aliasMap = new AliasMapValue { Name = property.GetAlias() };
+            if (Depth == 0)
+                RootAliasMap.Add(name, aliasMap);
+            else
+                _stack.Peek().ChildMap.Add(name, aliasMap);
+
+            bool hasChildren = property is IObjectProperty;
+            if (hasChildren)
+                _stack.Push(aliasMap);
+        }
+    }
+}
--- src/Foundatio.Repositories.Elasticsearch/Extensions/ElasticExtensions.cs
+++ src/Foundatio.Repositories.Elasticsearch/Extensions/ElasticExtensions.cs
@@ -1,13 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using Elasticsearch.Net;
 using Nest;
 
 namespace Foundatio.Repositories.Elasticsearch.Extensions {
     public static class ElasticExtensions {
-        private static readonly Lazy<PropertyInfo> _connectionSettingsProperty = new Lazy<PropertyInfo>(() => typeof(HttpConnection).GetProperty("ConnectionSettings", BindingFlags.NonPublic | BindingFlags.Instance));
+        private const string ALIAS_KEY = "@@alias";
+
+        public static TDescriptor Alias<TDescriptor>(this TDescriptor descriptor, string alias) where TDescriptor : IDescriptor {
+            if (descriptor == null)
+                throw new ArgumentNullException(nameof(descriptor));
+
+            if (alias == null)
+                throw new ArgumentNullException(nameof(alias));
+
+            var property = descriptor as IProperty;
+            if (property == null)
+                throw new ArgumentException($"{nameof(descriptor)} must implement {nameof(IProperty)} to use aliases", nameof(descriptor));
+
+            if (property.LocalMetadata == null)
+                property.LocalMetadata = new Dictionary<string, object>();
+
+            property.LocalMetadata.Add(ALIAS_KEY, alias);
+            return descriptor;
+        }
+
+        public static string GetAliasFromDescriptor<TDescriptor>(this TDescriptor descriptor) where TDescriptor : IDescriptor {
+            if (descriptor == null)
+                throw new ArgumentNullException(nameof(descriptor));
+
+            var property = descriptor as IProperty;
+            return property?.GetAlias();
+        }
+
+        public static string GetAlias(this IProperty property) {
+            if (property == null)
+                throw new ArgumentNullException(nameof(property));
+
+            if (property.LocalMetadata == null)
+                return null;
+
+            object alias;
+            if (property.LocalMetadata.TryGetValue(ALIAS_KEY, out alias))
+                return (string)alias;
+
+            return null;
+        }
 
         public static string GetErrorMessage(this IApiCallDetails response) {
             var sb = new StringBuilder();
