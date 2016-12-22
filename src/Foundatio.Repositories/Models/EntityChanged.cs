883ac8e3f1959011253b1245d51aa98880311148
Blake Niemyjski <bniemyjski@gmail.com>
2016-12-22 12:47:09 -0600

    Added editor config

--- src/Foundatio.Repositories/Models/EntityChanged.cs
+++ src/Foundatio.Repositories/Models/EntityChanged.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using Foundatio.Utility;
 
 namespace Foundatio.Repositories.Models {
+    [DebuggerDisplay("{Type} {ChangeType}: Id={Id}")]
     public class EntityChanged {
         public EntityChanged() {
             Data = new DataDictionary();
