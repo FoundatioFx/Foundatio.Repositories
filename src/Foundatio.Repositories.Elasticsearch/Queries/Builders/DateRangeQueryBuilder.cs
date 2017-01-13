283bd7528497f4801e15c90f29b280b60e270fd9
Blake Niemyjski <bniemyjski@gmail.com>
2017-01-12 20:50:29 -0600

    Fixed bug where soft deletes field name wasn't being inferred

--- src/Foundatio.Repositories.Elasticsearch/Queries/Builders/DateRangeQueryBuilder.cs
+++ src/Foundatio.Repositories.Elasticsearch/Queries/Builders/DateRangeQueryBuilder.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Foundatio.Parsers.LuceneQueries.Visitors;
 using Foundatio.Repositories.Elasticsearch.Queries.Options;
 using Foundatio.Utility;
 using Nest;
--- src/Foundatio.Repositories.Elasticsearch/Queries/Builders/SoftDeletesQueryBuilder.cs
+++ src/Foundatio.Repositories.Elasticsearch/Queries/Builders/SoftDeletesQueryBuilder.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Foundatio.Repositories.Elasticsearch.Queries.Options;
+using Foundatio.Repositories.Models;
 using Foundatio.Repositories.Queries;
 using Nest;
 
 namespace Foundatio.Repositories.Elasticsearch.Queries.Builders {
     public class SoftDeletesQueryBuilder : IElasticQueryBuilder {
+        private const string IsDeleted = nameof(ISupportSoftDeletes.IsDeleted);
+
         public Task BuildAsync<T>(QueryBuilderContext<T> ctx) where T : class, new() {
             var softDeletesQuery = ctx.GetSourceAs<ISoftDeletesQuery>();
             if (softDeletesQuery == null || softDeletesQuery.IncludeSoftDeletes)
                 return Task.CompletedTask;
 
-            var idsQuery = ctx.GetSourceAs<IIdentityQuery>();
-
-            var opt = ctx.GetOptionsAs<IElasticQueryOptions>();
-            if (opt == null || !opt.SupportsSoftDeletes || (idsQuery != null && idsQuery.Ids.Count > 0))
+            var options = ctx.GetOptionsAs<IElasticQueryOptions>();
+            if (options == null || !options.SupportsSoftDeletes)
                 return Task.CompletedTask;
 
-            // TODO: This needs to support different inferred names.
-            ctx.Filter &= new TermQuery {  Field = "isDeleted", Value = softDeletesQuery.IncludeSoftDeletes };
+            var idsQuery = ctx.GetSourceAs<IIdentityQuery>();
+            if (idsQuery != null && idsQuery.Ids.Count > 0)
+                return Task.CompletedTask;
 
+            string fieldName = options.IndexType?.GetFieldName(IsDeleted) ?? IsDeleted;
+            ctx.Filter &= new TermQuery {  Field = fieldName, Value = softDeletesQuery.IncludeSoftDeletes };
             return Task.CompletedTask;
         }
     }
