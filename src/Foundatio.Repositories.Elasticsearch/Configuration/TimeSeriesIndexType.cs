e1c15e7527e37826120b8b59e906a86b27b25312
Eric J. Smith <eric@codesmithtools.com>
2017-03-13 13:58:10 -0800

    Fix tests not being run

--- src/Foundatio.Repositories.Elasticsearch/Configuration/TimeSeriesIndexType.cs
+++ src/Foundatio.Repositories.Elasticsearch/Configuration/TimeSeriesIndexType.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Foundatio.Repositories.Elasticsearch.Queries;
 using Foundatio.Repositories.Extensions;
 using Foundatio.Repositories.Models;
 using Foundatio.Repositories.Options;
