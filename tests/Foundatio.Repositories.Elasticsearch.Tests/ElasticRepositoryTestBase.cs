8e92822c736444a35a545e3089c05682fcb6612f
Eric J. Smith <eric@codesmithtools.com>
2019-02-09 12:28:07 -0600

    Add waitforready method. Use it in tests.

--- tests/Foundatio.Repositories.Elasticsearch.Tests/ElasticRepositoryTestBase.cs
+++ tests/Foundatio.Repositories.Elasticsearch.Tests/ElasticRepositoryTestBase.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Foundatio.Caching;
 using Foundatio.Jobs;
 using Foundatio.Logging.Xunit;
@@ -27,6 +29,7 @@ namespace Foundatio.Repositories.Elasticsearch.Tests {
             _workItemQueue = new InMemoryQueue<WorkItemData>(new InMemoryQueueOptions<WorkItemData> { LoggerFactory = Log });
             _configuration = new MyAppElasticConfiguration(_workItemQueue, _cache, _messageBus, Log);
             _client = _configuration.Client;
+            _configuration.WaitForReady(new CancellationTokenSource(TimeSpan.FromMinutes(1)).Token);
         }
 
         protected virtual async Task RemoveDataAsync(bool configureIndexes = true) {
