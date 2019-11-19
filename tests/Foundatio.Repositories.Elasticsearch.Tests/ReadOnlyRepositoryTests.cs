9dda0949b013edf8a23ab28b2326211827cc210e
Eric J. Smith <eric@codesmithtools.com>
2019-11-19 11:02:43 -0600

    Another attempt

--- tests/Foundatio.Repositories.Elasticsearch.Tests/ReadOnlyRepositoryTests.cs
+++ tests/Foundatio.Repositories.Elasticsearch.Tests/ReadOnlyRepositoryTests.cs
@@ -690,7 +690,7 @@ namespace Foundatio.Repositories.Elasticsearch.Tests {
             Assert.Equal(1, results.Total);
 
             var localNow = dateTimeOffset.ToLocalTime().DateTime;
-            results = await _employeeRepository.GetByQueryAsync(o => o.DateRange(localNow.SubtractHours(1), localNow, "next", "America/Chicago"));
+            results = await _employeeRepository.GetByQueryAsync(o => o.DateRange(localNow.SubtractHours(1), localNow, "next", TimeZoneInfo.Local.Id));
 
             Assert.NotNull(results);
             Assert.Equal(1, results.Documents.Count);
