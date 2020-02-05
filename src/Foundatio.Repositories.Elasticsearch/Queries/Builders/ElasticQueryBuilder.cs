67b813589bab7ab789955eda18a39e9d7a96f243
Eric J. Smith <eric@codesmithtools.com>
2020-02-05 11:36:55 -0600

    Adding priority sorting to query builders

--- src/Foundatio.Repositories.Elasticsearch/Queries/Builders/ElasticQueryBuilder.cs
+++ src/Foundatio.Repositories.Elasticsearch/Queries/Builders/ElasticQueryBuilder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Foundatio.Parsers.ElasticQueries;
 using Foundatio.Parsers.LuceneQueries.Visitors;
@@ -7,7 +9,9 @@ using Foundatio.Repositories.Extensions;
 
 namespace Foundatio.Repositories.Elasticsearch.Queries.Builders {
     public class ElasticQueryBuilder : IElasticQueryBuilder {
-        private readonly List<IElasticQueryBuilder> _partBuilders = new List<IElasticQueryBuilder>();
+        private readonly List<ElasticQueryBuilderRegistration> _registrations = new List<ElasticQueryBuilderRegistration>();
+        private IElasticQueryBuilder[] _queryBuilders = null;
+
 
         public ElasticQueryBuilder(bool registerDefaultBuilders = true) {
             if (registerDefaultBuilders)
@@ -18,27 +22,74 @@ namespace Foundatio.Repositories.Elasticsearch.Queries.Builders {
             Register(new T(), replace);
         }
 
+        public void Register<T>(int priority, bool replace = true) where T : IElasticQueryBuilder, new() {
+            Register(new T(), replace, priority);
+        }
+
         public void Register(params IElasticQueryBuilder[] builders) {
+            foreach (var builder in builders)
+                Register(new ElasticQueryBuilderRegistration(builder));
+        }
+
+        public void Register(params ElasticQueryBuilderRegistration[] builders) {
             foreach (var builder in builders)
                 Register(builder);
         }
 
-        public void Register<T>(T builder, bool replace = true) where T : IElasticQueryBuilder {
+        public void Register(ElasticQueryBuilderRegistration registration) {
+            if (_queryBuilders != null)
+                throw new InvalidOperationException("Can not modify query builder registrations after first use.");
+            
+            _registrations.Add(registration);
+        }
+
+        public void Register(IElasticQueryBuilder builder, int priority) {
+            Register(new ElasticQueryBuilderRegistration(builder, priority));
+        }
+
+        public void Register<T>(T builder, bool replace = true, int? priority = null) where T : IElasticQueryBuilder {
+            if (_queryBuilders != null)
+                throw new InvalidOperationException("Can not modify query builder registrations after first use.");
+            
             if (replace) {
-                int existing = _partBuilders.FindIndex(b => b.GetType() == typeof(T));
-                if (existing >= 0)
-                    _partBuilders.RemoveAt(existing);
+                int existing = _registrations.FindIndex(b => b.Builder.GetType() == typeof(T));
+                if (existing >= 0) {
+                    if (priority == null)
+                        priority = _registrations[existing].Priority;
+                    _registrations.RemoveAt(existing);
+                }
             }
 
-            _partBuilders.Add(builder);
+            Register(new ElasticQueryBuilderRegistration(builder, priority));
+        }
+
+        public void RegisterBefore<T>(IElasticQueryBuilder builder) {
+            int priority = 0;
+            var referenceBuilder = _registrations.FirstOrDefault(v => typeof(T) == v.Builder.GetType());
+            if (referenceBuilder != null)
+                priority = referenceBuilder.Priority - 1;
+            
+            Register(new ElasticQueryBuilderRegistration(builder, priority));
+        }
+
+        public void RegisterAfter<T>(IElasticQueryBuilder builder) {
+            int priority = 0;
+            var referenceBuilder = _registrations.FirstOrDefault(v => typeof(T) == v.Builder.GetType());
+            if (referenceBuilder != null)
+                priority = referenceBuilder.Priority + 1;
+            
+            Register(new ElasticQueryBuilderRegistration(builder, priority));
         }
 
         public bool Unregister<T>() where T : IElasticQueryBuilder {
-            int existing = _partBuilders.FindIndex(b => b.GetType() == typeof(T));
+            if (_queryBuilders != null)
+                throw new InvalidOperationException("Can not modify query builder registrations after first use.");
+
+            int existing = _registrations.FindIndex(b => b.Builder.GetType() == typeof(T));
             if (existing < 0)
                 return false;
 
-            _partBuilders.RemoveAt(existing);
+            _registrations.RemoveAt(existing);
 
             return true;
         }
@@ -72,11 +123,26 @@ namespace Foundatio.Repositories.Elasticsearch.Queries.Builders {
         }
 
         public async Task BuildAsync<T>(QueryBuilderContext<T> ctx) where T : class, new() {
-            foreach (var builder in _partBuilders)
+            if (_queryBuilders == null)
+                Interlocked.CompareExchange(ref _queryBuilders, _registrations.OrderBy(v => v.Priority).Select(r => r.Builder).ToArray(), null);
+            
+            foreach (var builder in _queryBuilders)
                 await builder.BuildAsync(ctx).AnyContext();
         }
 
         private static readonly Lazy<ElasticQueryBuilder> _default = new Lazy<ElasticQueryBuilder>(() => new ElasticQueryBuilder());
         public static ElasticQueryBuilder Default => _default.Value;
+    }
+    
+    public class ElasticQueryBuilderRegistration {
+        private static int _currentAutoPriority;
+
+        public ElasticQueryBuilderRegistration(IElasticQueryBuilder builder, int? priority = null) {
+            Builder = builder;
+            Priority = priority ?? Interlocked.Increment(ref _currentAutoPriority);
+        }
+        
+        public IElasticQueryBuilder Builder { get; }
+        public int Priority { get; }
     }
 }
