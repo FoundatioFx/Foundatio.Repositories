9337cde8c2c75a3f118fdbea65e32b74a25f8f6c
Eric J. Smith <eric@codesmithtools.com>
2021-07-19 16:16:59 -0500

    Use explicit migration index mappings

--- src/Foundatio.Repositories.Elasticsearch/Repositories/MigrationStateRepository.cs
+++ src/Foundatio.Repositories.Elasticsearch/Repositories/MigrationStateRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Foundatio.Repositories.Elasticsearch.Configuration;
+using Foundatio.Repositories.Elasticsearch.Extensions;
 using Foundatio.Repositories.Migrations;
 using Nest;
 
@@ -15,6 +16,17 @@ namespace Foundatio.Repositories.Elasticsearch {
 
         public MigrationIndex(IElasticConfiguration configuration, string name = "migration", int replicas = 1) : base(configuration, name) {
             _replicas = replicas;
+        }
+
+        public override TypeMappingDescriptor<MigrationState> ConfigureIndexMapping(TypeMappingDescriptor<MigrationState> map) {
+            return map
+                .Dynamic(false)
+                .Properties(p => p
+                    .Keyword(f => f.Name(e => e.Id))
+                    .Number(f => f.Name(e => e.Version).Type(NumberType.Integer))
+                    .Date(f => f.Name(e => e.StartedUtc))
+                    .Date(f => f.Name(e => e.CompletedUtc))
+                );
         }
 
         public override CreateIndexDescriptor ConfigureIndex(CreateIndexDescriptor idx) {
